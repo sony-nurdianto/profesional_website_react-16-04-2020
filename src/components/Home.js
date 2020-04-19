d568a4c54ac9aed2b548bf50e14080105f5cbe26
sony-nurdianto <sonynurdianto445@gmail.com>
2020-04-19 22:03:30 +0700

    perbaikan

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
-import Logo from './images/logo.png'
-import Menu from './images/open-menu.png'
+import Html from './images/html5.png'
+import Css from './images/css3.png'
 import Nodejs from './images/nodejs.png'
 import Lreact from './images/logo.svg'
 import Js from './images/jjs.png'
-import Py from './images/py.png'
+import Mongodb from './images/mongodb.png'
+import Sql from './images/sql.png'
 import './style.css'
 
 
@@ -14,7 +15,7 @@ export default class Home extends Component {
    
     render() {
         return (
-            <div className="text-white bg">
+            <div className="text-white  bg">
                 {/* <div className="jumbotron jumbotron-fluid bg-transparent " style={{height : "30px"}}>
                     <div className="container">
                         <div className="row d-flex align-items-center">
@@ -37,9 +38,9 @@ export default class Home extends Component {
                     </div>
                 </div> */}
                     {/* <div className="jumbptron jumbotron-fluid mb-5 mt-5 pb-5 "> */}
-                        <div className="container ps">
+                        <div className="container ">
                             <div className="row">
-                                <div className="col text-center " style={{width :" 50px"}}>
+                                <div className="col text-center mt-5 mb-5" style={{width :" 50px"}}>
                                     <h1 className="Title">Are you looking for Web Developer</h1>
                                     <h1 className="Title2">I'am here Ready to help you</h1>
                                 </div>
@@ -62,25 +63,69 @@ export default class Home extends Component {
                            </div>
                        </div>
                     </div>
-                    <div className="container mb-5 mt-5 pb-5">
+                    <div className="container mb-5 mt-5 pb-5 text">
                         <div className="row">
-                            <div className="col float-left bg-dark">
-                                <h1>Lorem ipsum</h1>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <div className="col d-flex align-items-center">
+                                <img src={Nodejs} style={{width :" 20em"}}/>
                             </div>
-                            <div className="col d-flex justify-content-end align-items-center">
-                                    <img src={Nodejs} style={{width :" 20em"}}/>
+                            <div className="col float-left bg-dark ">
+                            <h1>Lorem ipsum</h1>
+                                <p className="text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>  
                             </div>
                         </div>
                     </div>
-                    <div className="container mb-5 mt-5 pb-5">
+                    <div className="container mb-5 mt-5 pb-5 text">
                         <div className="row">
-                            <div className="col">
+                                <div className="col bg-dark">
+                            <h1 className="text">Lorem ipsum</h1>
+                                <p className="text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+                            <div className="col d-flex justify-content-end">
                                 <img src={Js} style={{width :" 20em", height :"19em"}}/>
                             </div>
+                        </div>
+                    </div>
+                    <div className="container mb-5 mt-5 pb-5 text">
+                        <div className="row">
+                            <div className="col">
+                                <img src={Html} style={{width :" 20em", height :"19em"}}/>
+                            </div>
                             <div className="col bg-dark">
-                            <h1>Lorem ipsum</h1>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <h1 className="text">Lorem ipsum</h1>
+                                <p className="text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container mb-5 mt-5 pb-5 text">
+                        <div className="row">
+                            <div className="col bg-dark">
+                                <h1 className="text">Lorem ipsum</h1>
+                                <p className="text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+                            <div className="col d-flex justify-content-end">
+                                <img src={Css} style={{width :" 20em", height :"19em"}}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container mb-5 mt-5 pb-5 text">
+                        <div className="row">
+                            <div className="col">
+                                <img src={Mongodb} style={{width :" 20em", height :"19em"}}/>
+                            </div>
+                            <div className="col bg-dark">
+                                <h1 className="text">Lorem ipsum</h1>
+                                <p className="text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container mb-5 mt-5 pb-5 text">
+                        <div className="row">
+                            <div className="col bg-dark">
+                                <h1 className="text">Lorem ipsum</h1>
+                                <p className="text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+                            <div className="col d-flex justify-content-end">
+                                <img src={Sql} style={{width :" 20em", height :"19em"}}/>
                             </div>
                         </div>
                     </div>
