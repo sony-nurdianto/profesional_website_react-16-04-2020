e101c1a838b0fefe1635728feeaaca50afe0a38d
sony-nurdianto <sonynurdianto445@gmail.com>
2020-07-08 21:12:00 +0700

    push

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,9 +11,9 @@ const Footer = () => {
             <div className="footer">
                 <div className="big mt-5">
                     <div className="footer-icon">
-                        <a href="www.linkedin.com/in/sony-nurdianto-a831571a7"><i className="fa fa-linkedin-square h1 icon" aria-hidden="true" ></i>linkind</a >
-                        <a href="https://github.com/sony-nurdianto"> <i className="fa fa-github-alt h1 icon" aria-hidden="true"></i>github</a>
-                        <a href="https://wa.me/6285646303062" ><i className="fa fa-whatsapp h1 icon" aria-hidden="true"></i>whatsapp</a>
+                        <a href="www.linkedin.com/in/sony-nurdianto-a831571a7"><i className="fa fa-linkedin-square h1 icon" aria-hidden="true" ></i></a >
+                        <a href="https://github.com/sony-nurdianto"> <i className="fa fa-github-alt h1 icon" aria-hidden="true"></i></a>
+                        <a href="https://wa.me/6285646303062" ><i className="fa fa-whatsapp h1 icon" aria-hidden="true"></i></a>
                     </div>
                     <div className="waterMark mt-2">
                         <h6>Sony Nurdianto <span>@2020</span></h6>
